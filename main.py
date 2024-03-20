9fd8c97fcca342af1f6ae93c07afa97ae9ca9a05
dannychemm123 <dannychemm121@gmail.com>
2024-03-20 14:16:51 +0000

    Data Ingestion 1

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
 from cnnClassifier import logger
+from cnnClassifier.pipeline.data_ingestion_stage_01 import DataIngestionTrainingPipeline
+
+STAGE_NAME = "DATA INGESTION STAGE"
 
 
-logger.info("Welcome to cnnClassifier")
+
+try:
+    logger.info(f'>>>>>>> Stage {STAGE_NAME}<<<<<<<< Started Successfully')
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f'>>>>>>> Stage {STAGE_NAME}<<<<<<<< Completed Successfully\n\nx=======x')
+except Exception as e:
+    logger.exception(e)
---
+++ src/cnnClassifier/pipeline/data_ingestion_stage_01.py
@@ -0,0 +1,32 @@
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier.components.data_ingestion import DataIngestion
+from cnnClassifier import logger
+
+STAGE_NAME = "DATA INGESTION STAGE"
+
+class DataIngestionTrainingPipeline:
+    ''' Training pipeline for training data'''
+    def __ini__(self):
+        pass
+    
+    
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion = DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_data_file()
+        data_ingestion.extract_zip_file()
+    
+
+
+if __name__ == 'main:':
+    try:
+        logger.info(f'>>>>>>> Stage {STAGE_NAME}<<<<<<<< Started Successfully')
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f'>>>>>>> Stage {STAGE_NAME}<<<<<<<< Completed Successfully\n\nx=======x')
+    except Exception as e:
+        logger.exception(e)
+        
+        
+    
